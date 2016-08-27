9300ffb9ddf07739ae243530319924d13974f3a2
Emerson Cantalice <emersoncantalicee@gmail.com>
2016-08-27 00:32:39 -0300

    Commit inicial - Sigelar - Estrutura Basica Aplicaçao

---
+++ Banco de Dados/CargaUsuario.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO usuario(id, name, password, role, username)
+    VALUES (1, 'Emerson Cantalice', '123', 'ROLE_ADMIN', 'escantalice');
+INSERT INTO usuario(id, name, password, role, username)
+    VALUES (2, 'Doglas Lima', '123', 'ROLE_USER', 'dlima');
