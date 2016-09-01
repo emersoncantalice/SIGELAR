37a8a52fc859c1d2406b77c469ad0c297a2f93cd
DoglasProg <dogllas12@hotmail.com>
2016-09-01 20:50:49 -0300

    Insert de usuario

--- Banco de Dados/CargaUsuario.sql
+++ Banco de Dados/CargaUsuario.sql
@@ -1,3 +1,3 @@
 insert into usuario
-    values (1, '1990-01-01', '1990-01-01', 'Emerson Cantalice', '123', 'ROLE_ADMIN', 'escantalice'),
-	  	   (2, '1990-01-01', '1990-01-01', 'Doglas Lima', '123', 'ROLE_USER', 'dlima');
+    values (1, '1990-01-01', '1990-01-01', 'Emerson Cantalice', '123', 'ROLE_ADMIN', 'escantalice','emersoncantalicee@gmail.com'),
+	  	   (2, '1990-01-01', '1990-01-01', 'Doglas Lima', '123', 'ROLE_USER', 'shinigami', 'dogllas12@hotmail.com');
