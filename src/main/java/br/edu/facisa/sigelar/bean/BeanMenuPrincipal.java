44cb751f118ea0a50ffeabe888ae7740e632efae
Emerson Cantalice <emersoncantalicee@gmail.com>
2016-09-28 19:29:55 -0300

    Correção imports

--- src/main/java/br/edu/facisa/sigelar/bean/BeanMenuPrincipal.java
+++ src/main/java/br/edu/facisa/sigelar/bean/BeanMenuPrincipal.java
@@ -1,9 +1,6 @@
 package br.edu.facisa.sigelar.bean;
 
-import java.io.IOException;
 import java.io.Serializable;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -11,7 +8,6 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
-import org.omnifaces.util.Faces;
 import org.omnifaces.util.Messages;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
@@ -27,8 +23,6 @@ public class BeanMenuPrincipal implements Serializable {
 
 	private String nomeUsuario;
 	private String ultimoAcesso;
-	private String hostname;
-	private String ip;
 	private Usuario usuario;
 
 	@PostConstruct
@@ -45,18 +39,12 @@ public class BeanMenuPrincipal implements Serializable {
 				usuario.setDataUltimoAcesso(usuario.getDataAcessoAtual());
 				usuario.setDataAcessoAtual(new Date());
 				SimpleDateFormat formata = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-
 				ultimoAcesso = formata.format(usuario.getDataUltimoAcesso());
 				nomeUsuario = usuario.getNome().substring(0, usuario.getNome().indexOf(" "));
 
-				InetAddress addr = null;
-				addr = InetAddress.getLocalHost();
-
-				hostname = addr.getHostName();
-				ip = addr.getHostAddress();
 				usuarioDAO.merge(usuario);
 			}
-		} catch (UnknownHostException e) {
+		} catch (Exception e) {
 			Messages.addGlobalError("Erro ao carregar a página inicial");
 		}
 	}
@@ -75,22 +63,6 @@ public class BeanMenuPrincipal implements Serializable {
 
 	public void setUltimoAcesso(String ultimoAcesso) {
 		this.ultimoAcesso = ultimoAcesso;
-	}
-
-	public String getHostname() {
-		return hostname;
-	}
-
-	public void setHostname(String hostname) {
-		this.hostname = hostname;
-	}
-
-	public String getIp() {
-		return ip;
-	}
-
-	public void setIp(String ip) {
-		this.ip = ip;
 	}
 
 	public Usuario getUsuario() {
