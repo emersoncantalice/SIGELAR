df67e06666b18e1223076d2a30322bc5a9254afc
DoglasProg <dogllas12@hotmail.com>
2016-10-24 17:55:59 -0300

    Modificação no leyaut do banco de dados, e remoção de excesso de imports

--- src/main/java/br/edu/facisa/sigelar/domain/RegraAcesso.java
+++ src/main/java/br/edu/facisa/sigelar/domain/RegraAcesso.java
@@ -3,9 +3,6 @@ package br.edu.facisa.sigelar.domain;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
